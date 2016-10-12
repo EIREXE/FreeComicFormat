0c1f51becf8cd52ab366079d3ef1c023c4337f8a
eirexe123@gmail.com <eirexe123@gmail.com>
2016-10-12 14:42:02 +0200

    Cleanup viewer code and make all work in memory.
    Signed-off-by: eirexe123@gmail.com <eirexe123@gmail.com>

--- freecomic.py
+++ freecomic.py
@@ -9,59 +9,52 @@ import os
 import json
 import zipfile
 
-
 class FreeComicCollection:
-    def __init__(self, path):
-        self.path = path
-        if not os.path.isfile(path):
-            raise IOError("File does not exist")
-        if not zipfile.is_zipfile(path):
-            raise IOError("File is invalid")
-        zipf = zipfile.ZipFile(path)
+    def __init__(self, path, create=False):
+        self.zip_path = path
+        if not create:
+            if not os.path.isfile(path):
+                raise IOError("File does not exist")
+            if not zipfile.is_zipfile(path):
+                raise IOError("File is invalid")
+        self.zip_file = zipfile.ZipFile(path, "a")
         self.comic_list = list()
-        for file_path in zipf.namelist():
-            if file_path.endswith(".json") and not "/" in file_path:
-                print(file_path)
-                json_file = zipf.open(file_path)
-                comic_config = FreeComicConfig.from_dict(json.loads(zipf.open(file_path).read().decode("utf-8")), path)
+        self.update_comics()
+
+    def update_comics(self):
+        for file_path in self.zip_file.namelist():
+            if file_path.endswith(".json") and "/" not in file_path:
+                comic_config = FreeComicConfig.from_dict(json.loads(self.zip_file.open(file_path).read().decode("utf-8")), self.zip_file, self.zip_path)
                 self.comic_list.append(comic_config)
 class FreeComicConfig:
-    def __init__(self, series_names, episode_names, languages, short_name, episode_number, number_of_pages, zip_path):
+    def __init__(self, series_names, episode_names, languages, short_name, episode_number, number_of_pages, zip_file, zip_path):
         self.series_names = series_names
         self.episode_names = episode_names
         self.languages = languages
         self.short_name = short_name
         self.episode_number = episode_number
-        self.number_of_pages = number_of_pages
+        self.number_of_pages = number_of_pages  # Should start from one, because comic artists are not programmers, right?
+        self.zip_file = zip_file
         self.zip_path = zip_path
     @staticmethod
-    def from_dict(dictionary, zip_path):
-        return FreeComicConfig(dictionary["series_names"],dictionary["episode_names"], dictionary["languages"], dictionary["short_name"], dictionary["episode_number"], dictionary["number_of_pages"], zip_path)
-    def get_page_base(self, page_number):
-        if not os.path.isfile(self.zip_path):
-            raise IOError("File does not exist")
-        if not zipfile.is_zipfile(self.zip_path):
-            raise IOError("File is invalid")
+    def from_dict(dictionary, zip_file, zip_path):
+        return FreeComicConfig(dictionary["series_names"], dictionary["episode_names"], dictionary["languages"],
+                               dictionary["short_name"], dictionary["episode_number"], dictionary["number_of_pages"],
+                               zip_file, zip_path)
 
-        zipf = zipfile.ZipFile(self.zip_path)
-        return self.get_page_base_from_memory(zipf, page_number)
     def get_page_translation(self, language, page_number):
-        if not os.path.isfile(self.zip_path):
-            raise IOError("File does not exist")
-        if not zipfile.is_zipfile(self.zip_path):
-            raise IOError("File is invalid")
-
-        zipf = zipfile.ZipFile(self.zip_path)
-        return self.get_page_translation_from_memory(zipf, language, page_number)
-    def get_page_translation_from_memory(self, zipfile, language, page_number):
-        path = "{0}/{1}/{2}/{3}.svg".format(self.short_name,self.episode_number,language ,page_number)
-        if path in zipfile.namelist():
-            return zipfile.open(path).read()
+        path = "{0}/{1}/{2}/{3}.svg".format(self.short_name, self.episode_number, language, page_number)
+        if path in self.zip_file.namelist():
+            return self.zip_file.open(path).read()
         else:
             raise IOError("Translation does not exist")
-    def get_page_base_from_memory(self, zipfile, page_number):
-        path = "{0}/{1}/base/{2}.png".format(self.short_name,self.episode_number, page_number)
-        if path in zipfile.namelist():
-            return zipfile.open(path).read()
+
+    def get_page_base(self, page_number):
+        path = "{0}/{1}/base/{2}.png".format(self.short_name, self.episode_number, page_number)
+        print(path)
+        print(self.zip_file.namelist())
+        if path in self.zip_file.namelist():
+            return self.zip_file.open(path).read()
         else:
             raise IOError("Page does not exist")
+
--- viewer.py
+++ viewer.py
@@ -7,26 +7,26 @@
 import os
 import gi
 import sys
-import copy
-import time
 import zipfile
 from freecomic import FreeComicCollection
 
-
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, GLib, Gdk
-from gi.repository.GdkPixbuf import Pixbuf, InterpType, PixbufLoader
+from gi.repository import Gtk, Gdk
+from gi.repository.GdkPixbuf import InterpType, PixbufLoader
+
 
 class Handler:
-
     def __init__(self):
         self.previous_allocation_height = 0
         self.previous_allocation_width = 0
-        self.comic_language = "en"
-        self.new_page_in = False
+        self.comic_language = "es"  # TODO: Fix this
+        self.new_page_in = False  # This is True when a new page comes in.
         self.current_page = 0
-        self.comic = False
-        self.comic_zip = False
+        self.comic = False  # FreeComicCollection
+        self.comic_zip = False  # Comic zip file
+        self.comic_base_pixbuf = None  # Base pixbuf
+        self.translated_comic_pixbuf = None  # Fullres pixbuf with translations added
+        self.scaled_pixbuf = None  # Pixbuf scaled to size
 
     def load_base(self, number):
         # Loads the base page (aka the page without the speech bubbles) from file.
@@ -34,38 +34,41 @@ class Handler:
         # the size of the page, but we can't do that from memory apparently so now
         # we just assume that the svg has the proper width and height setup
         pixbuf_loader = PixbufLoader.new_with_type('png')
-        pixbuf_loader.write(self.comic.get_page_base_from_memory(self.comic_zip,number))
+        pixbuf_loader.write(self.comic.get_page_base(number))
         pixbuf_loader.close()
         pixbuf = pixbuf_loader.get_pixbuf()
-        self.comic_pixbuf = pixbuf
+        self.comic_base_pixbuf = pixbuf
 
     def load_translation(self, number):
-        # Gets the translation svg and loads it to memory, I used to use a differe
+        # Gets the translation svg and loads it to memory, I used to use a different method that allowed for scaling SVG
+        # files to the proper resolution (rendering it again), however this method sucks ass and the authors should just
+        # make their SVGs be the proper size.
         pixbuf_loader = PixbufLoader.new_with_type('svg')
-        pixbuf_loader.write(self.comic.get_page_translation_from_memory(self.comic_zip,self.comic_language, number))
+        pixbuf_loader.write(self.comic.get_page_translation(self.comic_language, number))
         pixbuf_loader.close()
-        print(pixbuf_loader.get_pixbuf())
         pixbuf = pixbuf_loader.get_pixbuf()
-        self.translated_comic_pixbuf = self.comic_pixbuf
-        pixbuf.composite(self.translated_comic_pixbuf, 0, 0, self.comic_pixbuf.props.width, self.comic_pixbuf.props.height, 0, 0, 1.0, 1.0, InterpType.HYPER,254)
+        self.translated_comic_pixbuf = self.comic_base_pixbuf
+        pixbuf.composite(self.translated_comic_pixbuf, 0, 0, self.comic_base_pixbuf.props.width,
+                         self.comic_base_pixbuf.props.height, 0, 0, 1.0, 1.0, InterpType.HYPER, 254)
 
     def on_draw(self, widget, cr):
         if self.comic:
             allocation = widget.get_allocation()
-            if self.temp_height != allocation.height or self.temp_width != allocation.width or self.new_page_in:
+            if self.previous_allocation_height != allocation.height or self.previous_allocation_width != allocation.width or self.new_page_in:
                 self.new_page_in = False
                 self.previous_allocation_height = allocation.height
                 self.previous_allocation_width = allocation.width
 
                 # This gets how much we have to scale the original full-size pixbuf when allocation size changes
-                difference_ratio = allocation.width/self.translated_comic_pixbuf.get_width()
-                new_height = self.translated_comic_pixbuf.get_height()*difference_ratio
-                new_width = self.translated_comic_pixbuf.get_width()*difference_ratio
-                self.scaled_pixbuf = self.translated_comic_pixbuf.scale_simple(new_width, new_height, InterpType.BILINEAR)
-                # If we don't request a specific height the drawingarea won't draw properly
+                difference_ratio = allocation.width / self.translated_comic_pixbuf.get_width()
+                new_height = self.translated_comic_pixbuf.get_height() * difference_ratio
+                new_width = self.translated_comic_pixbuf.get_width() * difference_ratio
+                self.scaled_pixbuf = self.translated_comic_pixbuf.scale_simple(new_width, new_height,
+                                                                               InterpType.BILINEAR)
+                # If we don't request a specific height the drawing area won't draw properly
                 widget.set_size_request(-1, self.scaled_pixbuf.get_height())
             # This does the actual drawing
-            Gdk.cairo_set_source_pixbuf(cr, self.scaled_pixbuf, 0,0)
+            Gdk.cairo_set_source_pixbuf(cr, self.scaled_pixbuf, 0, 0)
             cr.paint()
 
     def set_comic(self, collection):
@@ -78,6 +81,7 @@ class Handler:
 
         self.comic_zip = zipfile.ZipFile(self.comic.zip_path)
         self.set_page(0)
+
     def set_page(self, page):
         # Changes comic page
         if self.comic:
@@ -88,57 +92,60 @@ class Handler:
             self.current_page = page
             self.force_draw = True
             self.draw_area.queue_draw()
+
     def on_back(self, widget):
-        if not self.current_page-1 < 0:
-            self.set_page(self.current_page-1)
+        if not self.current_page - 1 < 0:
+            self.set_page(self.current_page - 1)
 
     def open_file(self, widget):
         chooser_dialog = Gtk.FileChooserDialog(title="Open file"
-                                               ,action=Gtk.FileChooserAction.OPEN
-                                               ,buttons=["Open", Gtk.ResponseType.OK, "Cancel", Gtk.ResponseType.CANCEL]
-        )
-        pyFilter = Gtk.FileFilter()
-        pyFilter.add_pattern('*.fcb')
-        pyFilter.set_name('Free comic book file')
-        chooser_dialog.add_filter(pyFilter)
+                                               , action=Gtk.FileChooserAction.OPEN
+                                               ,
+                                               buttons=["Open", Gtk.ResponseType.OK, "Cancel", Gtk.ResponseType.CANCEL]
+                                               )
+        filter = Gtk.FileFilter()
+        filter.add_pattern('*.fcb')
+        filter.set_name('Free comic book file')
+        chooser_dialog.add_filter(filter)
         response = chooser_dialog.run()
-
+        # Did we chose a not correct file? let's find out.
         if response == Gtk.ResponseType.OK:
-            print("ok")
             filename = chooser_dialog.get_filename()
             chooser_dialog.destroy()
             try:
                 self.set_comic(FreeComicCollection(filename))
             except Exception as e:
-                self.error_message(e)
+                self.show_error_message(e)
         chooser_dialog.destroy()
 
-    def error_message(self, exception):
-        dialog = Gtk.MessageDialog()
-        error = "Error in line {0}!,{1}".format(sys.exc_info()[-1].tb_lineno, str(exception))
-        messagedialog = Gtk.MessageDialog(parent=None,
-                                          flags=Gtk.DialogFlags.MODAL,
-                                          type=Gtk.MessageType.ERROR,
-                                          buttons=Gtk.ButtonsType.OK_CANCEL,
-                                          message_format=error)
-        messagedialog.connect("response", self.dialog_response)
-        messagedialog.show()
-    def dialog_response(self, widget, response_id):
+    def show_error_message(self, exception):
+        error = "Error in line {0}!,{1}, call a programmer.".format(sys.exc_info()[-1].tb_lineno, str(exception))
+        print(error)
+        message_dialog = Gtk.MessageDialog(parent=None,
+                                           flags=Gtk.DialogFlags.MODAL,
+                                           type=Gtk.MessageType.ERROR,
+                                           buttons=Gtk.ButtonsType.OK_CANCEL,
+                                           message_format=error)
+        message_dialog.connect("response", self.dialog_response_callback)
+        message_dialog.show()
+
+    def dialog_response_callback(self, widget, response_id):
         widget.destroy()
 
     def on_forward_pressed(self, widget):
-        if not self.current_page+1 > self.comic.number_of_pages:
-            self.set_page(self.current_page+1)
+        if not self.current_page + 1 > self.comic.number_of_pages:
+            self.set_page(self.current_page + 1)
 
-builder = Gtk.Builder()
-builder.add_from_file("viewertest.glade")
-
+    def on_window_close(self, widget, what):
+        Gtk.main_quit()
 handler = Handler()
-
+builder = Gtk.Builder()
+builder.add_from_file("viewer.glade")
 builder.connect_signals(handler)
 
 window = builder.get_object("ViewerWindow")
 handler.draw_area = builder.get_object("ComicArea")
+
 window.show_all()
 
 Gtk.main()
